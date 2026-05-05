c6ed871b0af0a07c509347251bb5e2a87adc02d7
maianh-233 <162794036+maianh-233@users.noreply.github.com>
2026-05-05 15:50:43 +0700

    Xay dung he thong folder va file

---
+++ backend/promotionservice/promotionservice/src/main/java/com/fashion/promotionservice/PromotionserviceApplication.java
@@ -0,0 +1,13 @@
+package com.fashion.promotionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromotionserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromotionserviceApplication.class, args);
+	}
+
+}
